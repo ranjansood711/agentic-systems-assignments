f9890d2974daab3d94f06169d56a5cc049b9d80b
ranjansood711 <ranjansood711@gmail.com>
2026-03-06 21:21:05 +0530

    FastAPI Fundamentals - Subjective Assignment completed

--- fastapi-fundamentals/main.py
+++ fastapi-fundamentals/main.py
@@ -1,6 +1,15 @@
+#pip install fastapi pydantic uvicorn
+#uvicorn main:app --reload
+
 from fastapi import FastAPI, HTTPException
-import json
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/search")
+def search(name: Optional[str] = None, age: Optional[int] = None):
+    # FastAPI automatically converts this dictionary into a JSON response
+    return {
+        "name": name,
+        "age": age
+    }
